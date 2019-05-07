31f5744db5e0e6f329e2fb1e386a6505f931e4db
mskhquiz <50354916+mskhquiz@users.noreply.github.com>
2019-05-07 10:47:28 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function check(q, a, t, b) {
+    document.getElementsByClassName("answer" + q)[t].style.background = "#29a329";
+    document.getElementsByClassName("answer" + q)[t].style.borderColor = "#29a329";
+    document.getElementsByClassName("answer" + q)[t].style.color = "#fff";
+    document.getElementById("points").innerText = parseInt(document.getElementById("points").innerText)+1;
+    if (b == false) {
+        document.getElementsByClassName("answer" + q)[a].style.background = "#cc1211";
+        document.getElementsByClassName("answer" + q)[a].style.borderColor = "#cc1211";
+        document.getElementsByClassName("answer" + q)[a].style.color = "#fff";
+        document.getElementById("points").innerText = parseInt(document.getElementById("points").innerText)-1;
+    }
+    for (var i = 0; i<4; i++) {
+        document.getElementsByClassName("answer"+q)[i].disabled = true;
+    }
+}
